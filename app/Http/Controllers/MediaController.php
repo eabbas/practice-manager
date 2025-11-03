79c1674ded9b8e1b47bce0b09ad4212966124dd1
samin <saminshoafiii@gmail.com>
2025-11-03 17:30:42 +0330

    media 01

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -6,17 +6,5 @@ use Illuminate\Http\Request;
 
 class MediaController extends Controller
 {
-    public function index(){
-        return view("media.upload");
-    }
-
-    public function upload(Request $request){
-
-        $originalExtentsion = request()->file->getClientOriginalExtentsion();
-        $originalName = request()->file->getClientOriginalName();
-        $fileName = $originalName;
-
-
-
-    }
+    // 
 }
--- resources/views/practice/create.blade.php
+++ resources/views/practice/create.blade.php
@@ -20,8 +20,9 @@
          <option value="{{$lesson->id}}">{{$lesson->title}}</option>
         @endforeach
        </select>
-
-     <input type="file" multiple>
+       <!-- <input type="file" name = "practiceMedia">
+       <label>:فایل ضمیمه</label> -->
+       
        <button>create</button>
     </form>
 </body>
